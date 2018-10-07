3ad9fc90f91714537ff116361d29ceb34179e332
Samuel Klein <samuel.klein.mail@web.de>
2018-10-07 17:43:17 +0200

    changes

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ https.createServer(options, function(req, res){
   for(var i in config.websites){
     var website = config.websites[i];
     if(subdomain == website.subdomain){
-      //proxy_http.web(req, res, {target: "https://"+URL+":"+website.port_https});
-      //flag = true;
+      proxy_http.web(req, res, {target: "https://"+URL+":"+website.port_https});
+      flag = true;
       console.log("Subdomain found:\t"+subdomain);
       break;
     }
