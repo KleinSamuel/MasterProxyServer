514ca430200bc9e978df110a5f0f7d599b76b4f3
Samuel Klein <it@samuel-klein.net>
2020-03-14 12:06:44 +0100

    refactored and added route for non subdomain request

--- server.js
+++ server.js
@@ -3,60 +3,86 @@ const http = require("http");
 const https = require("https");
 const httpProxy = require("http-proxy");
 
-var config = require("./master.config");
+const config = require("./master.config");
 
-var URL = config.url;
-var PORT_HTTP = config.port_http;
-var PORT_HTTPS = config.port_https;
+let URL = config.url;
+let PORT_HTTP = config.port_http;
+let PORT_HTTPS = config.port_https;
 
-var options = {
-  key: fs.readFileSync(config.ssl_key, "utf-8"),
-  cert: fs.readFileSync(config.ssl_cert, "utf-8")
+let options = {
+    key: readFileSync(config.ssl_key),
+    cert: readFileSync(config.ssl_cert)
 };
 
-var proxy_http = httpProxy.createProxyServer({});
-var proxy_https = httpProxy.createProxyServer({
-  ssl: {
-    key: options.key,
-    cert: options.cert
-  },
-  secure: false
+function readFileSync(path){
+    try {
+        return fs.readFileSync(path, "utf-8")
+    } catch (error) {
+        return undefined
+    }
+};
+
+const proxy_http = httpProxy.createProxyServer({});
+const proxy_https = httpProxy.createProxyServer({
+    ssl: {
+        key: options.key,
+        cert: options.cert
+    },
+    secure: false
 });
 
-http.createServer(function(req, res){
-  var subdomain = req.headers.host.split(".")[0];
-  var flag = false;
-  for(var i in config.websites){
-    var website = config.websites[i];
-    if(subdomain == website.subdomain){
-      proxy_http.web(req, res, {target: "http://"+URL+":"+website.port_http});
-      flag = true;
-      break;
+http.createServer(function(req, res) {
+
+    let subdomain = req.headers.host.split(".")[0];
+    let flag = false;
+
+    if (subdomain === URL) {
+        proxy_http.web(req, res, {target: "http://"+URL});
+        flag = true;
+    } else {
+        for (let i in config.websites) {
+            let website = config.websites[i];
+            if (subdomain === website.subdomain) {
+                proxy_http.web(req, res, {target: "http://"+URL+":"+website.port_http});
+                flag = true;
+                break;
+            }
+        }
     }
-  }
-  if(!flag){
-    res.statusCode = 404;
-    res.end("<h1 style='margin-left:40%;margin-top:200px;'>404 Page Not Found</h1>");
-  }
-}).listen(PORT_HTTP, function(){
-  console.log("[ INFO ] HTTP Master Server running on port <"+PORT_HTTP+">");
+
+    if (!flag) {
+        res.statusCode = 404;
+        res.end("<h1 style='margin-left:40%;margin-top:200px;'>404 Page Not Found</h1>");
+    }
+
+}).listen(PORT_HTTP, function() {
+    console.log("[ INFO ] HTTP Master Server running on port <"+PORT_HTTP+">");
 });
 
 https.createServer(options, function(req, res){
-  var subdomain = req.headers.host.split(".")[0];
-  var flag = false;
-  for(var i in config.websites){
-    var website = config.websites[i];
-    if(subdomain == website.subdomain){
-      proxy_https.web(req, res, {target: "https://"+URL+":"+website.port_https});
-      flag = true;
-      break;
+
+    let subdomain = req.headers.host.split(".")[0];
+    let flag = false;
+
+    if (subdomain === URL) {
+        proxy_http.web(req, res, {target: "http://"+URL});
+        flag = true;
+    } else {
+        for (let i in config.websites) {
+            let website = config.websites[i];
+            if (subdomain === website.subdomain) {
+                proxy_https.web(req, res, {target: "https://"+URL+":"+website.port_https});
+                flag = true;
+                break;
+            }
+        }
     }
-  }
-  if(!flag){
-    res.statusCode = 404;
-    res.end("<h1 style='margin-left:40%;margin-top:200px;'>404 Page Not Found</h1>");
-  }
+
+    if(!flag){
+        res.statusCode = 404;
+        res.end("<h1 style='margin-left:40%;margin-top:200px;'>404 Page Not Found</h1>");
+    }
+
 }).listen(PORT_HTTPS, function(){
-  console.log("[ INFO ] HTTPS Master Server running on port <"+PORT_HTTPS+">");
+    console.log("[ INFO ] HTTPS Master Server running on port <"+PORT_HTTPS+">");
 });
